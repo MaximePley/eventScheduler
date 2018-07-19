56660c25897c5de897ddf23e61346a63e6aff27c
Maxime Pley <pleymaxime@gmail.com>
2018-07-19 19:56:59 +0100

    route development

--- publicapi.py
+++ publicapi.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from werkzeug.exceptions import NotFound, ServiceUnavailable
+from werkzeug.security import generate_password_hash, check_password_hash
 import json
-from flask import redirect, url_for, request, jsonify, abort, make_response
+from flask import render_template, redirect, url_for, request, jsonify, abort, make_response
 import requests
 import os
 import logging
@@ -27,9 +28,6 @@ def setupLogger(logLevel='INFO', filename='execution.log'):
     return setupLogger
 
 
-app = Flask(__name__)
-
-
 def root_dir():
     """ Returns root director for this project """
     return os.path.dirname(os.path.realpath(__file__))
@@ -41,9 +39,12 @@ def nice_json(arg):
     return response
 
 
+app = Flask(__name__)
+
+
 @app.route("/", methods=['GET'])
 def index():
-    return "<h1>welcome in the Event scheduler</h1>"
+    return "<h1>Welcome in the Event scheduler</h1>"
 
 
 @app.route("/<user>/events", methods=['GET'])
@@ -62,6 +63,51 @@ def user(user):
     output = {'user': responses[0], 'events': responses[1]}
 
     return jsonify(output)
+
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        user = requests.get("http://localhost:5001/users/{}".format(username))
+        password_hash = json.loads(user.text)["password_hash"]
+
+        if not check_password_hash(password_hash, request.form['password']):
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect('/{}/events'.format(username))
+    return render_template('login.html', error=error)
+
+
+# Route for handling the login page logic
+@app.route('/<username>/events', methods=['GET', 'POST'])
+def events(username):
+    error = None
+    if request.method == 'POST':
+        user = requests.post("http://localhost:5002/events?title=" + request.form['title'] + "&content=" + request.form['content'] + "&startDate=" + request.form['startDate'] + "&endDate=" + request.form['endDate'] + "&recurrence=" + request.form['recurrence'] + "&user=" + username)
+
+        return redirect('/{}/events'.format(username))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+# Route for handling the login page logic
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        if request.form['password'] != request.form['password2']:
+            error = 'Passwords dont match. Please try again.'
+        else:
+            requests.post("http://localhost:5001/register?username=" + request.form['username'] + "&email=" + request.form['email'] + "&password=" + request.form['password'])
+            return redirect(url_for('login'))
+    return render_template('register.html', error=error)
 
 
 if __name__ == "__main__":
